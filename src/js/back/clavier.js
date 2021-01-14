cf2a9da1efc1e948700ef5bbccdc556733028beb
ms-brahim <brahim7khalil@gmail.com>
2021-01-14 14:53:44 +0100

    Organize a clavier.js

--- src/js/back/clavier.js
+++ src/js/back/clavier.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 let dateFormat = window.require("dateformat");
 const swal = require('sweetalert2');
-var num = document.getElementById("outputNum");
 
 function call(){
-    let outputNum = num.innerHTML;
+    let outputNum = document.getElementById("outputNum").innerHTML;
     let dataArray = [outputNum.trim() +"<br><small>"+ dateFormat("d-mm-yyyy HH:mm:ss")+"</small>;"+"\n"];
     
     if (outputNum.length<1) {
@@ -14,14 +13,11 @@ function call(){
             if (err) throw err;
             console.log("Success Calling !!");
         });
-        calling.addEventListener('click',function()  {
-            let outputNum = num.innerHTML;
-            swal.fire(
-                "Calling...",
-                outputNum.trim(),
-                'success'
-            )
-        });
+       
+        swal.fire(
+            "Calling...",
+            outputNum.trim(),
+        );
     }
 };
 
